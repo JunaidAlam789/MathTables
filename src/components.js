36f4b9afb02255d3c1f6b1c9f0db8b93187cedad
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2022-11-17 21:23:31 +0500

    Add files via upload

--- src/components.js
+++ src/components.js
@@ -1,11 +1,13 @@
 //import { render } from '@testing-library/react'
 //import { toHaveFocus } from '@testing-library/jest-dom';
-import React from 'react'
+import React, { useEffect } from 'react'
 const useState= React.useState
 
   
      export function TableForm(props) {
         const [number, setNumber]=useState(props.value)
+        const [userAns, setUserAns]= useState()
+        const [userAnswer, setUserAnswer]=useState([])
         const [keys, setKeys]=useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
         const [numb1, setNumb1]=useState()
         const [numb2, setNumb2]=useState()
@@ -19,6 +21,11 @@ const useState= React.useState
         const [numb10, setNumb10]=useState() 
         
         //const [numb, setNumb]=useState([4,8,12,16,20,24,28,32,36,40])
+        useEffect(()=>{
+            setNumber(number)
+
+        },[number])
+
         const [numb, setNumb]=useState([,,,,,,,,,])
         const [ans, setAns]=useState([,,,,,,,,,])
          let i=[0,1,2,3,4,5,6,7,8,9];
@@ -32,6 +39,8 @@ const useState= React.useState
          setNumber(number)
          setNumb([,,,,,,,,,])
          setAns([,,,,,,,,,])
+         setUserAns('')
+         setUserAnswer(['','','','','','','','','',''])
          
         
         }
@@ -39,8 +48,16 @@ const useState= React.useState
         f.preventDefault()
         let count=0;
         ans.map((result)=> {if(result=="correct"){count++}})
-        if(count===10){alert("Excellent !!!")}
-        else(alert("Try Again"))
+        if(count===10){
+            alert("Excellent !!!")
+            setAns([,,,,,,,,,])
+            setNumb([,,,,,,,,,])
+            setUserAnswer(['','','','','','','','','',''])
+        }
+        else {
+            alert("Try Again")
+            
+        }
        
        }
         function onChangeInput1(e,index){
@@ -59,15 +76,20 @@ const useState= React.useState
         if(index==10) setNumb10(e.target.value); 
         setAns(setStates(ans,index,e.target.value))
         setNumb(setStates(numb,index,e.target.value))
+        setUserAnswer(setStates(numb,index,e.target.value))
          
         let answer=(number*(index+1));
         if(answer== e.target.value){ setStates(ans,index,"correct")}
         else { setStates(ans,index,"wrong")}
-        
-
-        
+         
         
             
+        }
+
+        function onChangeNumberReset(){
+            setAns([,,,,,,,,,])
+            setNumb([,,,,,,,,,])
+            setUserAnswer(['','','','','','','','','',''])
         }
         
                 
@@ -77,7 +99,7 @@ const useState= React.useState
         <form key="1234" onSubmit={formEventHandler1}>
             <fieldset>
                 <legend> Table </legend>
-            <input onChange={e => setNumber(e.target.value)} placeholder="Number for Table"></input>
+            <input value={userAns} onChange={e => {setNumber(e.target.value); setUserAns(e.target.value); onChangeNumberReset();}} placeholder="Number for Table"></input>
             <button>Submit</button>
             </fieldset>
         </form>
@@ -86,7 +108,7 @@ const useState= React.useState
         <fieldset>
         {i.map((index)=>{return (
        
-        <h3 key={"12"+keys[index]}>{number} x {index+1} = <input onChange={(e)=>onChangeInput1(e,index)} keys={13+keys[index+10]} index={index}  placeholder="Ans"></input> {ans[index]} </h3> 
+        <h3 key={"12"+keys[index]}>{number} x {index+1} = <input value={userAnswer[index]} onChange={(e)=>onChangeInput1(e,index)} keys={13+keys[index+10]} index={index}  placeholder="Ans"></input> {ans[index]} </h3> 
        
         ) })}
         <button>Submit</button>
